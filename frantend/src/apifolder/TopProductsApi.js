838b124dc0a22a2a92a38b04849cc893d22eda32
dyaasnc <dyaasenouci@gmail.com>
2022-08-06 17:43:22 +0200

    add search bar functionality

--- frantend/src/apifolder/TopProductsApi.js
+++ frantend/src/apifolder/TopProductsApi.js
@@ -2,7 +2,7 @@ export const ApiTopPrdcts = [
   {
     id: 1,
     src: "https://images-na.ssl-images-amazon.com/images/I/81au7a6yf6L._SL1500_.jpg",
-    product:"Housse",
+    product:"housse",
     price: 5800,
     disc: "house standard pour tt type de vehicules",
     quantity: 1,
@@ -12,7 +12,7 @@ export const ApiTopPrdcts = [
   {
     id: 2,
     src: "https://ae01.alicdn.com/kf/HTB1BI1sSVXXXXXcXXXXq6xXFXXXB/De-luxe-3D-Surround-Complet-Voiture-Housse-de-Si-ge-En-Cuir-PU-Si-ge-Couvre.jpg",
-    product:"Housse cuir",
+    product:"housse cuir",
     price: 14000,
     disc: "",
     quantity: 1,
@@ -26,7 +26,7 @@ export const ApiTopPrdcts = [
     price: 2000,
     disc: "",
     quantity: 1,
-    totalQty : 100,
+    totalQty : 0,
     category: 'accessoire interieur'
   },
    {
@@ -42,7 +42,7 @@ export const ApiTopPrdcts = [
    {
      id: 5,
     src: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.autopratic.com%2Fuploads%2Ficones-pages%2F168.jpg&f=1&nofb=1",
-    product:"Produit Nettoyage Tableau De Bord",
+    product:"produit Nettoyage Tableau De Bord",
     price: 2000,
     disc: "",
     quantity: 1,
@@ -52,7 +52,7 @@ export const ApiTopPrdcts = [
   {
     id: 6,
     src: "http://www.autoprolavage.fr/46/hot-rims-nettoyant-jantes-pneus-meguiar-s.jpg",
-    product:"Produit Nettoyage jantes",
+    product:"produit Nettoyage jantes",
     price: 2000,
     disc: "",
     quantity: 1,
